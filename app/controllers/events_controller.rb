9393e619fd35c43b8146c43c82edbfef31707d7b
Borna Safai <borna@paradem.co>
2016-02-14 18:28:24 -0500

    Update events
    Simple edit form

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,4 +2,15 @@ class EventsController < ApplicationController
   def index
     @events = Event.where('id > ?', 1).order(:startYear, :startMonth, :startDay, :endYear, :endMonth, :endDay)
   end
+
+  def edit
+    @event = Event.find(params[:id])
+  end
+
+  def update
+    @event = Event.find(params[:id])
+    event_params = params.require(:event).permit(:startYear, :startMonth, :startDay, :endYear, :endMonth, :endDay, :headline, :text, :media, :mediaCredit, :mediaCaption)
+    @event.update(event_params)
+    redirect_to events_path
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,10 @@ Rails.application.routes.draw do
   get "events" => "events#index"
   get "timeline" => "timeline#index"
   get "presentation" => "presentation#index"
+  get "events/:id/edit" => "events#edit", as: "edit_event"
+  get "events/:id" => "events#show", as: "event"
+
+  patch "events/:id" => "events#update"
 
   root "timeline#index"
 end
