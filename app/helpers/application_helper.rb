e69ef1e6965472bb139a53986ae06a1bab641d64
Abdullah Norozi Iranzad <a1norozi@gmail.com>
2016-02-20 20:14:34 -0600

    #14 nav active state

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def nav_link(link_text, link_path, extra_classes = nil, link_classes = nil)
+    class_name = current_page?(link_path) ? 'is-current' : nil
+
+    content_tag(:li, :class => [class_name, extra_classes]) do
+      link_to(link_text, link_path, :class => [link_classes])
+    end
+  end
 end
