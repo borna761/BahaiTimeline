928f545188a5cb14198be253e83a3e41a79a9836
Borna Safai <borna@paradem.co>
2016-02-22 16:51:16 -0500

    Updated history and timeline
    Resolves #4 and #12

--- app/controllers/timeline_controller.rb
+++ app/controllers/timeline_controller.rb
@@ -2,6 +2,6 @@ class TimelineController < ApplicationController
   def index
     @title = Eventtype.find_by_eventtype('Title').events.first
     @cycles = Eventtype.find_by_eventtype('Era').events.order(:startYear)
-    @events = Eventtype.find_by_eventtype('Event').events.order(:startYear, :startMonth, :startDay, :endYear, :endMonth, :endDay)
+    @events = Eventtype.find_by_eventtype('Event').events.order(startYear: :asc, startMonth: :asc, startDay: :asc, endYear: :desc, endMonth: :desc, endDay: :desc)
   end
 end
